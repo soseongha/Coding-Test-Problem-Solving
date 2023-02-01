b0f25384fb315699d48f2f458d17598862b416e6
sosongha3 <sosongha3@ajou.ac.kr>
2023-02-01 17:39:20 +0900

    first_commit

---
+++ baekjoon_11720/baekjoon_11720.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    int N = 0;
+    cin >> N;
+
+    string numbers;
+    cin >> numbers;
+
+    int total = 0;
+
+    for(int i = 0; i < N; i++)
+    { 
+        total += numbers[i] - '0';
+    }
+
+    cout << total <<"\n";
+
+}
